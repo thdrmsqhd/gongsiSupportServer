3d6896ddfc241678b6da8eaea7b0ca9a83e31474
song <thdrmsqhd@gmail.com>
2021-09-24 16:42:22 +0900

    [스프링버전 변경] 3->4

--- gongsiSupportServer/src/main/java/kr/kaisys/mockTest/controller/MockTestController.java
+++ gongsiSupportServer/src/main/java/kr/kaisys/mockTest/controller/MockTestController.java
@@ -1,12 +1,17 @@
 package kr.kaisys.mockTest.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 import kr.kaisys.mockTest.service.MockTestService;
 
@@ -18,17 +23,18 @@ public class MockTestController {
 	
 	
 	@RequestMapping("/getOneMockTest")
-	public void mockTestMapper(HttpServletRequest request) throws IOException {
+	@ResponseBody
+	public Map<String,String> mockTestMapper(HttpServletRequest request) throws IOException {
 		String id = (String) request.getParameter("id");
 		String pwd = (String) request.getParameter("pwd");
 		
 		String title = (String) request.getParameter("title");
 		String memo = (String) request.getParameter("memo");
 		
-		System.out.println(request.getAttributeNames());
-		System.out.println(request.getInputStream().toString());
-		
 		System.out.println(id + " : " + pwd + " : " + title + " : " + memo);
 		System.out.println(service.getOneMockTest());
+		Map<String,String> map = new HashMap<>();
+		map.put("param", "param");
+		return map;
 	}
 }
